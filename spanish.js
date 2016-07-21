c0b8d155781f9bb0c229db80512285e50b92f01a
Casey Reed <casey.w.reed@gmail.com>
2016-07-21 13:12:50 -0500

    Removed extraneous unworking code from spanish.js

--- spanish.js
+++ spanish.js
@@ -1,20 +1,9 @@
-(function translateToSpanish(userString) {
+function translateToSpanish(userString) {
   console.log("YO");
   console.log(userString)
-
-  for (let i = 0; i < userString.length; i++) {
-
-    if (userString[i] === "merry") {
-      userString[i] = "feliz"
-    }
+}
 
 
-
-  }
-
-}());
-
-
-var spanishdict = {feliz : "merry", navidad : "christmas",
+var spanishDict = {feliz : "merry", navidad : "christmas",
 fiestas : "holiday", feliz : "happy", nuevo : "new", año : "year",
 y : "and", un : "a", con : "with", amor : "love", de : "from"}
