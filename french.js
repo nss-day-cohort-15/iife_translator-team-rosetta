62ff7322fa6188ac4c63e9a6c8306e10b36a45a5
Anulfo <noaharkling@gmail.com>
2016-07-21 16:08:28 -0500

    the french translator engine

---
+++ french.js
@@ -0,0 +1,16 @@
+/*var splitedUserStr = ["happy", "new", "year"]
+var spanishDict = {merry : "joyeux", christmas : "noël", 
+holiday : "vacances", happy : "bonne", new : "nouveu", year : "année",
+and : "et", a : "une", with : "avec", love : "amour", from : "de"}               
+var finalStrFrench = [];
+
+for (i = 0; i < splitedUserStr.length; i++) {
+  var allKeys = Object.keys(spanishDict);
+  for (j = 0; j < allKeys.length; j++) {
+    if (splitedUserStr[i] === allKeys[j]) {
+      finalStrFrench.push(spanishDict[allKeys[j]]); 
+  }
+  
+  }
+}
+console.log(finalStrFrench.join(" "));*/
