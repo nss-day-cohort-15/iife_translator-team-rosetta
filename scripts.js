8c5e3cd92002530dea88f26542b073bae38b3fb4
Casey Reed <casey.w.reed@gmail.com>
2016-07-21 11:21:40 -0500

    Working buttons for determine language function

--- scripts.js
+++ scripts.js
@@ -1,4 +1,23 @@
-document.getElementById("translateButton").addEventListener("click", determineLanguage)
+(function determineLanguage () {
+  document.getElementById("translateButton").addEventListener("click", determineLanguage)
+  var userString = document.getElementById("userString").value;
+  var spanish = document.getElementById("spanishRadio");
+  var french = document.getElementById("frenchRadio");
+
+  if (spanish.checked) {
+    console.log("Spanish is checked")
+    // translateToSpanish(userString)
+    return userString
+  }
+
+    if (french.checked) {
+    console.log("French is checked")
+    // translateToFrench(userString)
+    return userString
+  }
+
+}());
+
 
 // determineLanguage FUNCTION (
 // var translateString = document.getElementById("userString").value
@@ -12,3 +31,6 @@ document.getElementById("translateButton").addEventListener("click", determineLa
 //      frenchTranslator (string);
 //      return string
 // }
+
+// (function() {
+// }());
