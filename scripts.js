1fd337f0945d9a8bfcb69fdfac92be44ab427885
Casey Reed <casey.w.reed@gmail.com>
2016-07-21 13:07:57 -0500

    Baby steps to getting the spanish translator to work

--- scripts.js
+++ scripts.js
@@ -1,10 +1,18 @@
-var translate = (function determineLanguage () {
+
+
+var translate = (
+
+  function determineLanguage () {
   document.getElementById("translateButton").addEventListener("click", determineLanguage)
+  document.addEventListener("keydown", enter);
+  function enter(e) {
+    if (13 == e.keyCode) {
+       determineLanguage()
+    }
+  }
   var userString = document.getElementById("userString").value.split(" ");
   var spanish = document.getElementById("spanishRadio");
   var french = document.getElementById("frenchRadio");
-
-  console.log(userString)
 
   if (spanish.checked) {
     console.log("Spanish is checked")
---
+++ spanish.js
@@ -0,0 +1,20 @@
+(function translateToSpanish(userString) {
+  console.log("YO");
+  console.log(userString)
+
+  for (let i = 0; i < userString.length; i++) {
+
+    if (userString[i] === "merry") {
+      userString[i] = "feliz"
+    }
+
+
+
+  }
+
+}());
+
+
+var spanishdict = {feliz : "merry", navidad : "christmas",
+fiestas : "holiday", feliz : "happy", nuevo : "new", año : "year",
+y : "and", un : "a", con : "with", amor : "love", de : "from"}
