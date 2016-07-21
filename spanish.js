bc997ff1c03e89fd9c4882dcb082536feceeab52
Anulfo <noaharkling@gmail.com>
2016-07-21 15:49:40 -0500

    the translator engine

--- spanish.js
+++ spanish.js
@@ -1,9 +1,16 @@
-function translateToSpanish(userString) {
-  console.log("YO");
-  console.log(userString)
+/*var spliteduserstr = []
+var spanishdict = {merry : "feliz", christmas : "navidad", 
+holiday : "fiestas", happy : "feliz", new : "nuevo", year : "año",
+and : "y", a : "un", with : "con", love : "amor", from : "de"}               
+var finalstr = [];
+
+for (i = 0; i < spliteduserstr.length; i++) {
+  var allKeys = Object.keys(spanishdict);
+  for (j = 0; j < allKeys.length; j++) {
+    if (spliteduserstr[i] === allKeys[j]) {
+      finalstr.push(spanishdict[allKeys[j]]); 
+  }
+  
+  }
 }
-
-
-var spanishDict = {feliz : "merry", navidad : "christmas",
-fiestas : "holiday", feliz : "happy", nuevo : "new", año : "year",
-y : "and", un : "a", con : "with", amor : "love", de : "from"}
+console.log(finalstr.join(" "));*/
