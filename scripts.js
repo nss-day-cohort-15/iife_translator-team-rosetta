6c7f812ca8bc4805d9e0b4390a0fe0acdbf64d60
Casey Reed <casey.w.reed@gmail.com>
2016-07-21 10:45:35 -0500

    Wrote basic DOM js handler and outlined function map

---
+++ scripts.js
@@ -0,0 +1,14 @@
+document.getElementById("translateButton").addEventListener("click", determineLanguage)
+
+// determineLanguage FUNCTION (
+// var translateString = document.getElementById("userString").value
+//
+//if spanishRadio.checked {
+//      spansishTranslator (string);
+//      return string
+// }
+//
+// if frenchRadio.checked {
+//      frenchTranslator (string);
+//      return string
+// }
