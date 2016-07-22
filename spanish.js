2a4c56b65a3266c1a77faa5ebbc1d7d17de6ee46
Casey Reed <casey.w.reed@gmail.com>
2016-07-21 20:22:34 -0500

    Added consolelog checks and rearranged dictionary to be more logical

--- spanish.js
+++ spanish.js
@@ -1,9 +1,23 @@
 Router = (function (router) {
 
 
-  var spanishDict = {feliz : "merry", navidad : "christmas",
-  fiestas : "holiday", feliz : "happy", nuevo : "new", año : "year",
-  y : "and", un : "a", con : "with", amor : "love", de : "from"}
+  // var spanishDict = {feliz : "merry", navidad : "christmas",
+  // fiestas : "holiday", feliz : "happy", nuevo : "new", año : "year",
+  // y : "and", un : "a", con : "with", amor : "love", de : "from"}
+
+  var spanishDict = {
+    merry: "feliz",
+    christmas: "navidad",
+    holiday: "fiesta",
+    happy: "feliz",
+    new: "nuevo",
+    year: "año",
+    and: "y",
+    a: "un",
+    with: "con",
+    love: "amor",
+    from: "de"
+  }
 
 
   // var translatedSpanish = []
@@ -12,16 +26,27 @@ Router = (function (router) {
   router.translateToSpanish = function(stringArr){
     console.log("YO translateToSpanish is running!");
 
-    // var stringArr = Router.getString();
-    console.log(stringArr);
+    var stringArr = Router.getString();
+    // console.log(stringArr);
     for (var i = 0; i < stringArr.length; i++) {
       console.log('inside the for loop');
+      var currentWord = stringArr[i]
+      for (var key in spanishDict) {
+        if (spanishDict[key] === stringArr[i]) {
+          console.log("INSIDE THE FOR IN LOOP!")
+          stringArr[i] = spanishDict[key]
+          console.log(stringArr[i], "HIT THE STRING REASSIGNMENT!")
+          console.log(spanishDict[key])
+          return stringArr
+        }
+      }
 
 
+  // console.log("spanishDict" + key + " = " + spanishDict[key]);
     }
-    console.log(router.translatedSpanish);
+    // console.log(router.translatedSpanish);
   }
-console.log(router)
+// console.log(router)
 // console.log('router.translatedSpanish', router.translatedSpanish)
 return router
 return userString
